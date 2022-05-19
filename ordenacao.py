9bd496c2d59f098b212132d4683894f26399e090
gabriborges <94412072+gabriborges@users.noreply.github.com>
2022-05-19 12:22:39 -0300

    Add files via upload

--- ordenacao.py
+++ ordenacao.py
@@ -2,135 +2,194 @@ import random
 from re import X
 import time
 
-lista_ordenada = [0]
+lista_ordenada20k = [0]
 x=[1]
 for i in range(0,19999):
-    lista_ordenada.extend(x)
+    lista_ordenada20k.extend(x)
     x[0]=x[0]+1  
 
-lista_inversa = [19999]
+lista_inversa20k = [19999]
 x=[19999]
 for i in range(0,19999):
-    lista_inversa.extend(x)
+    lista_inversa20k.extend(x)
     x[0]=x[0]-1  
 
-lista_aleatoria = random.sample(range(19999),19999)
+lista_aleatoria20k = random.sample(range(19999),19999)
+
+#-------------#
+
+lista_ordenada40k = [0]
+x=[1]
+for i in range(0,39999):
+    lista_ordenada40k.extend(x)
+    x[0]=x[0]+1  
+
+lista_inversa40k = [39999]
+x=[39999]
+for i in range(0,39999):
+    lista_inversa40k.extend(x)
+    x[0]=x[0]-1  
+
+lista_aleatoria40k = random.sample(range(39999),39999)
+
+#-------------#
+
+lista_ordenada60k = [0]
+x=[1]
+for i in range(0,59999):
+    lista_ordenada60k.extend(x)
+    x[0]=x[0]+1  
+
+lista_inversa60k = [59999]
+x=[59999]
+for i in range(0,59999):
+    lista_inversa60k.extend(x)
+    x[0]=x[0]-1  
+
+lista_aleatoria60k = random.sample(range(59999),59999)
+
+#-------------#
+
+lista_ordenada80k = [0]
+x=[1]
+for i in range(0,79999):
+    lista_ordenada80k.extend(x)
+    x[0]=x[0]+1  
+
+lista_inversa80k = [79999]
+x=[79999]
+for i in range(0,79999):
+    lista_inversa80k.extend(x)
+    x[0]=x[0]-1  
+
+lista_aleatoria80k = random.sample(range(79999),79999)
+
+#-------------#
+
+lista_ordenada100k = [0]
+x=[1]
+for i in range(0,79999):
+    lista_ordenada100k.extend(x)
+    x[0]=x[0]+1  
+
+lista_inversa100k = [79999]
+x=[79999]
+for i in range(0,79999):
+    lista_inversa100k.extend(x)
+    x[0]=x[0]-1  
+
+lista_aleatoria100k = random.sample(range(99999),99999)
+
+#-------------#
+
+
+
+def quick_sort(A):
+	quick_sort2(A, 0, len(A)-1)
+	
+def quick_sort2(A, low, hi):
+	if hi-low < 20 and low < hi:
+		quick_selection(A, low, hi)
+	elif low < hi:
+		p = partition(A, low, hi)
+		quick_sort2(A, low, p - 1)
+		quick_sort2(A, p + 1, hi)
+	
+def get_pivot(A, low, hi):
+	mid = (hi + low) // 2
+	s = sorted([A[low], A[mid], A[hi]])
+	if s[1] == A[low]:
+		return low
+	elif s[1] == A[mid]:
+		return mid
+	return hi
+	
+def partition(A, low, hi):
+	pivotIndex = get_pivot(A, low, hi)
+	pivotValue = A[pivotIndex]
+	A[pivotIndex], A[low] = A[low], A[pivotIndex]
+	border = low
+
+	for i in range(low, hi+1):
+		if A[i] < pivotValue:
+			border += 1
+			A[i], A[border] = A[border], A[i]
+	A[low], A[border] = A[border], A[low]
+
+	return (border)
+	
+def quick_selection(x, first, last):
+	for i in range (first, last):
+		minIndex = i
+		for j in range (i+1, last+1):
+			if x[j] < x[minIndex]:
+				minIndex = j
+		if minIndex != i:
+			x[i], x[minIndex] = x[minIndex], x[i]
 
 
 
 
 
 
-def quicksort(lista, inicio=0, fim=None):
-    if fim is None:
-        fim = len(lista)-1
-    if inicio < fim:
-        p = partition(lista, inicio, fim)
-        quicksort(lista, inicio, p-1)
-        quicksort(lista, p+1, fim)
+print("Lista de 20k: ")
+ini_20K = time.time()
+quick_sort(lista_ordenada20k)
+fim_20K = time.time()
 
-def partition(lista, inicio, fim):
-    pivot = lista[fim]
-    i = inicio
-    for j in range(inicio, fim):
-        if lista[j] <= pivot:
-            lista[j], lista[i] = lista[i], lista[j]
-            i = i + 1
-    lista[i], lista[fim] = lista[fim], lista[i]
-    return i
+ini3_20K = time.time()
+quick_sort(lista_aleatoria20k)
+fim3_20K = time.time()
+print("Tempo bubble Sort ordenada: "+ str( fim_20K - ini_20K))
+
+print("Tempo bubble Sort aleatoria: "+ str( fim3_20K - ini3_20K))
+
+print("Lista de 40k: ")
+ini_40K = time.time()
+quick_sort(lista_ordenada40k)
+fim_40K = time.time()
+
+ini3_40K = time.time()
+quick_sort(lista_aleatoria40k)
+fim3_40K = time.time()
+print("Tempo bubble Sort ordenada: "+ str( fim_40K - ini_40K))
+
+print("Tempo bubble Sort aleatoria: "+ str( fim3_40K - ini3_40K))
 
 
-def merge(lista, inicio, meio, fim):
-    left = lista[inicio:meio]
-    right = lista[meio:fim]
-    top_left, top_right = 0, 0
-    for k in range(inicio, fim):
-        if top_left >= len(left):
-            lista[k] = right[top_right]
-            top_right = top_right + 1
-        elif top_right >= len(right):
-            lista[k] = left[top_left]
-            top_left = top_left + 1
-        elif left[top_left] < right[top_right]:
-            lista[k] = left[top_left]
-            top_left = top_left + 1
-        else:
-            lista[k] = right[top_right]
-            top_right = top_right + 1
-def mergesort(lista, inicio=0, fim=None):
-    if fim is None:
-        fim = len(lista)
-    if(fim - inicio > 1):
-        meio = (fim + inicio)//2
-        mergesort(lista, inicio, meio)
-        mergesort(lista, meio, fim)
-        merge(lista, inicio, meio, fim)
+print("Lista de 60k: ")
+ini_60K = time.time()
+quick_sort(lista_ordenada60k)
+fim_60K = time.time()
 
-def insertion_sort(lista):
-    n = len(lista)
-    for i in range(1, n):
-        chave = lista[i]
-        j = i - 1
-        while j >= 0 and lista[j] > chave:
-            lista[j+1] = lista[j]
-            j = j - 1
-        lista[j+1] = chave
+ini3_60K = time.time()
+quick_sort(lista_aleatoria60k)
+fim3_60K = time.time()
+print("Tempo bubble Sort ordenada: "+ str( fim_60K - ini_60K))
 
-def bubble_sort(lista):
-    n = len(lista)
-    for j in range(n-1):
-        for i in range(n-1):
-            if lista[i] > lista[i+1]:
-                # troca de elementos nas posições i e i+1
-                lista[i], lista[i+1] = lista[i+1], lista[i]
+print("Tempo bubble Sort aleatoria: "+ str( fim3_60K - ini3_60K))
 
-def selection_sort(lista):
-    n = len(lista)
-    for j in range(n-1):
-        min_index = j
-        for i in range(j, n):
-            if lista[i] < lista[min_index]:
-                min_index = i
-        if lista[j] > lista[min_index]:
-            aux = lista[j]
-            lista[j] = lista[min_index]
-            lista[min_index] = aux
+print("Lista de 80k: ")
+ini_80K = time.time()
+quick_sort(lista_ordenada80k)
+fim_80K = time.time()
 
-ini_bubble = time.time()
-bubble_sort(lista_ordenada)
-fim_bubble = time.time()
-ini_bubble2 = time.time()
-bubble_sort(lista_inversa)
-fim_bubble2 = time.time()
-ini_bubble3 = time.time()
-bubble_sort(lista_aleatoria)
-fim_bubble3 = time.time()
-print("Tempo bubble Sort: "+ str( fim_bubble - ini_bubble))
-print("Tempo bubble Sort: "+ str( fim_bubble2 - ini_bubble2))
-print("Tempo bubble Sort: "+ str( fim_bubble3 - ini_bubble3))
-"""
-ini_inser = time.time()
-insertion_sort(lista_ordenada)
-fim_inser = time.time()
-ini_inser2 = time.time()
-insertion_sort(lista_inversa)
-fim_inser2 = time.time()
-ini_inser3 = time.time()
-insertion_sort(lista_aleatoria)
-fim_inser3 = time.time()
-print("Tempo Insertion Sort: "+ str( fim_inser - ini_inser))
-print("Tempo Insertion Sort: "+ str( fim_inser2 - ini_inser2))
-print("Tempo Insertion Sort: "+ str( fim_inser3 - ini_inser3))
+ini3_80K = time.time()
+quick_sort(lista_aleatoria80k)
+fim3_80K = time.time()
+print("Tempo bubble Sort ordenada: "+ str( fim_80K - ini_80K))
 
-ini_quicksort = time.time()
-quicksort(lista_ordenada)
-fim_quicksort = time.time()
-ini_quicksort2 = time.time()
-quicksort(lista_inversa)
-fim_quicksort2 = time.time()
-ini_quicksort3 = time.time()
-quicksort(lista_aleatoria)
-fim_quicksort3 = time.time()
-print("Tempo quicksort Sort: "+ str( fim_quicksort - ini_quicksort))
-print("Tempo quicksort Sort: "+ str( fim_quicksort2 - ini_quicksort2))
-print("Tempo quicksort Sort: "+ str( fim_quicksort3 - ini_quicksort3))"""
+print("Tempo bubble Sort aleatoria: "+ str( fim3_80K - ini3_80K))
+
+print("Lista de 100k: ")
+ini_100K = time.time()
+quick_sort(lista_ordenada100k)
+fim_100K = time.time()
+
+ini3_100K = time.time()
+quick_sort(lista_aleatoria100k)
+fim3_100K = time.time()
+print("Tempo ordenada: "+ str( fim_100K - ini_100K))
+
+print("Tempo aleatoria: "+ str( fim3_100K - ini3_100K))
+
--- quick.py
+++ quick.py
@@ -83,23 +83,64 @@ lista_aleatoria100k = random.sample(range(99999),99999)
 #-------------#
 
 
-def bubble_sort(lista):
-    n = len(lista)
-    for j in range(n-1):
-        for i in range(n-1):
-            if lista[i] > lista[i+1]:
-                # troca de elementos nas posições i e i+1
-                lista[i], lista[i+1] = lista[i+1], lista[i]
+
+def quick_sort(A):
+	quick_sort2(A, 0, len(A)-1)
+	
+def quick_sort2(A, low, hi):
+	if hi-low < 20 and low < hi:
+		quick_selection(A, low, hi)
+	elif low < hi:
+		p = partition(A, low, hi)
+		quick_sort2(A, low, p - 1)
+		quick_sort2(A, p + 1, hi)
+	
+def get_pivot(A, low, hi):
+	mid = (hi + low) // 2
+	s = sorted([A[low], A[mid], A[hi]])
+	if s[1] == A[low]:
+		return low
+	elif s[1] == A[mid]:
+		return mid
+	return hi
+	
+def partition(A, low, hi):
+	pivotIndex = get_pivot(A, low, hi)
+	pivotValue = A[pivotIndex]
+	A[pivotIndex], A[low] = A[low], A[pivotIndex]
+	border = low
+
+	for i in range(low, hi+1):
+		if A[i] < pivotValue:
+			border += 1
+			A[i], A[border] = A[border], A[i]
+	A[low], A[border] = A[border], A[low]
+
+	return (border)
+	
+def quick_selection(x, first, last):
+	for i in range (first, last):
+		minIndex = i
+		for j in range (i+1, last+1):
+			if x[j] < x[minIndex]:
+				minIndex = j
+		if minIndex != i:
+			x[i], x[minIndex] = x[minIndex], x[i]
+
+
+
+
+
 
 print("Lista de 20k: ")
 ini_20K = time.time()
-bubble_sort(lista_ordenada20k)
+quick_sort(lista_ordenada20k)
 fim_20K = time.time()
 ini2_20K = time.time()
-bubble_sort(lista_inversa20k)
+quick_sort(lista_inversa20k)
 fim2_20K = time.time()
 ini3_20K = time.time()
-bubble_sort(lista_aleatoria20k)
+quick_sort(lista_aleatoria20k)
 fim3_20K = time.time()
 print("Tempo bubble Sort ordenada: "+ str( fim_20K - ini_20K))
 print("Tempo bubble Sort inversa: "+ str( fim2_20K - ini2_20K))
@@ -107,13 +148,13 @@ print("Tempo bubble Sort aleatoria: "+ str( fim3_20K - ini3_20K))
 
 print("Lista de 40k: ")
 ini_40K = time.time()
-bubble_sort(lista_ordenada40k)
+quick_sort(lista_ordenada40k)
 fim_40K = time.time()
 ini2_40K = time.time()
-bubble_sort(lista_inversa40k)
+quick_sort(lista_inversa40k)
 fim2_40K = time.time()
 ini3_40K = time.time()
-bubble_sort(lista_aleatoria40k)
+quick_sort(lista_aleatoria40k)
 fim3_40K = time.time()
 print("Tempo bubble Sort ordenada: "+ str( fim_40K - ini_40K))
 print("Tempo bubble Sort inversa: "+ str( fim2_40K - ini2_40K))
@@ -122,13 +163,13 @@ print("Tempo bubble Sort aleatoria: "+ str( fim3_40K - ini3_40K))
 
 print("Lista de 60k: ")
 ini_60K = time.time()
-bubble_sort(lista_ordenada60k)
+quick_sort(lista_ordenada60k)
 fim_60K = time.time()
 ini2_60K = time.time()
-bubble_sort(lista_inversa60k)
+quick_sort(lista_inversa60k)
 fim2_60K = time.time()
 ini3_60K = time.time()
-bubble_sort(lista_aleatoria60k)
+quick_sort(lista_aleatoria60k)
 fim3_60K = time.time()
 print("Tempo bubble Sort ordenada: "+ str( fim_60K - ini_60K))
 print("Tempo bubble Sort inversa: "+ str( fim2_60K - ini2_60K))
@@ -136,28 +177,30 @@ print("Tempo bubble Sort aleatoria: "+ str( fim3_60K - ini3_60K))
 
 print("Lista de 80k: ")
 ini_80K = time.time()
-bubble_sort(lista_ordenada80k)
+quick_sort(lista_ordenada80k)
 fim_80K = time.time()
 ini2_80K = time.time()
-bubble_sort(lista_inversa80k)
+quick_sort(lista_inversa80k)
 fim2_80K = time.time()
 ini3_80K = time.time()
-bubble_sort(lista_aleatoria80k)
+quick_sort(lista_aleatoria80k)
 fim3_80K = time.time()
 print("Tempo bubble Sort ordenada: "+ str( fim_80K - ini_80K))
 print("Tempo bubble Sort inversa: "+ str( fim2_80K - ini2_80K))
 print("Tempo bubble Sort aleatoria: "+ str( fim3_80K - ini3_80K))
 
 print("Lista de 100k: ")
-ini_bubble_100K = time.time()
-bubble_sort(lista_ordenada100k)
-fim_bubble_100K = time.time()
-ini_bubble2_100K = time.time()
-bubble_sort(lista_inversa100k)
-fim_bubble2_100K = time.time()
-ini_bubble3_100K = time.time()
-bubble_sort(lista_aleatoria100k)
-fim_bubble3_100K = time.time()
-print("Tempo bubble Sort: "+ str( fim_bubble_100K - ini_bubble_100K))
-print("Tempo bubble Sort: "+ str( fim_bubble2_100K - ini_bubble2_100K))
-print("Tempo bubble Sort: "+ str( fim_bubble3_100K - ini_bubble3_100K))
+ini_100K = time.time()
+quick_sort(lista_ordenada100k)
+fim_100K = time.time()
+ini2_100K = time.time()
+quick_sort(lista_inversa100k)
+fim2_100K = time.time()
+ini3_100K = time.time()
+quick_sort(lista_aleatoria100k)
+fim3_100K = time.time()
+print("Tempo ordenada: "+ str( fim_100K - ini_100K))
+print("Tempo inversa: "+ str( fim2_100K - ini2_100K))
+print("Tempo aleatoria: "+ str( fim3_100K - ini3_100K))
+
+print(lista_aleatoria20k)
